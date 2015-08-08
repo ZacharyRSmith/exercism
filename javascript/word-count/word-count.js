2f8ed2a462cfe683a143183daa11856e19146d47
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-08 14:14:22 +0000

    Refactor word-count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,18 +1,15 @@
 module.exports = words;
 
-function addCount (elt, obj) {
-  if (obj[elt] == null || isNaN(obj[elt])) { obj[elt] = 1; }
-  else { obj[elt] += 1; }
-
-  return obj;
-}
-
 function words (str) {
   var words = str.match(/\S+/g);
   var wordCounts = {};
 
   words.forEach(function (word) {
-    wordCounts = addCount(word, wordCounts);
+    if (wordCounts.hasOwnProperty(word)) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
   });
 
   return wordCounts;
