6d255d5e496e37233ede934097966c1fa254df05
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-09 00:09:37 +0000

    Added a commented-out fxnl solution to etl

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,9 +1,19 @@
+// function transform (old) {
+//   return Object.keys(old).reduce(function (rslt, score) {
+//     old[score].forEach(function (ltr) {
+//       rslt[ltr.toLowerCase()] = +score;
+//     });
+
+//     return rslt;
+//   }, {});
+// }
+
 function transform (old) {
   var rslt = {};
 
   for (var score in old) {
     old[score].forEach(function (ltr) {
-      rslt[ltr.toLowerCase()] = parseInt(score);
+      rslt[ltr.toLowerCase()] = +score;
     });
   }
 
