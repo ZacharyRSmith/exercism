09a67110fbccf639941fc4c267d764babc064016
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-10 17:32:50 +0000

    Second iteration for python/hamming

---
+++ python/hamming/hamming.py
@@ -0,0 +1,11 @@
+def distance(a, b):
+    if len(a) != len(b):
+        raise ValueError("Humming Distance is only defined for strands of equal length")
+
+    distance = 0
+
+    for pair in zip(list(a), list(b)):
+        if pair[0] != pair[1]:
+            distance += 1
+
+    return distance
