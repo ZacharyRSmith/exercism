56dad330e870ada97cb69e685122d96af425d1b1
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-10 21:37:08 +0000

    Fix typo in hamming.py ValueError msg

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,6 +1,6 @@
 def distance(a, b):
     if len(a) != len(b):
-        raise ValueError("Humming Distance is only defined for strands of equal length")
+        raise ValueError("Hamming Distance is only defined for strands of equal length")
 
     distance = 0
 
