fb7ced24aa47e28a21d2cea8fc43c759dcab9952
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-11 19:14:52 +0000

    Add commented-out non-bitwise/binary solution to python/allergies

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -9,17 +9,23 @@ class Allergies (object):
     def _set_lst(self, allergy_score):
         lst = []
         allergens = {
-            'eggs': 1,
-            'peanuts': 2,
-            'shellfish': 4,
-            'strawberries': 8,
-            'tomatoes': 16,
-            'chocolate': 32,
-            'pollen': 64,
-            'cats': 128,
+            1: 'eggs',
+            2: 'peanuts',
+            4: 'shellfish',
+            8: 'strawberries',
+            16: 'tomatoes',
+            32: 'chocolate',
+            64: 'pollen',
+            128: 'cats',
         }
 
-        for a, score in allergens.iteritems():
+        # This is part of a non-bitwise/binary solution:
+#         for score, a in sorted(allergens.iteritems(), reverse=True):
+#             if allergy_score >= score:
+#                 lst.append(a)
+#                 allergy_score -= score
+
+        for score, a in allergens.iteritems():
             if score & allergy_score > 0:
                 lst.append(a)
 
