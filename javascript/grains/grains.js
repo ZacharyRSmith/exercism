f4b929d35e91113a58979a79080bfbf9c135c1fd
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-11 04:22:32 +0000

    Submission for javascript/grains

---
+++ javascript/grains/grains.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function Grains () {}
+
+Grains.prototype = {
+    constructor: Grains,
+
+    square: function (n) { return Math.pow(2, n-1); },
+
+    // The sum of squares is the square of the next int minus 1.
+    total: function () { return this.square(65) - 1; }
+}
+
+module.exports = Grains;
