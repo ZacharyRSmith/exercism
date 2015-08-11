29c89c9da4cc111ba4e1a1569f5af3cf892c6f93
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-11 15:23:46 +0000

    Second submission for python/anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,24 +1,9 @@
-def _are_same_chars(strA, strB):
-    strA_chars = list(strA)
-    strA_chars.sort()
-    strB_chars = list(strB)
-    strB_chars.sort()
-
-    return strA_chars == strB_chars
-
-
 def detect_anagrams(word, candidates):
-    word = word.lower()
     anagrams = []
 
     for c in candidates:
-        c_orig = c
-        c = c.lower()
-
-        if c == word:
-            continue
-
-        if _are_same_chars(word, c):
-            anagrams.append(c_orig)
+        if (c.lower() != word.lower()):
+            if sorted(c.lower()) == sorted(word.lower()):
+                anagrams.append(c)
 
     return anagrams
