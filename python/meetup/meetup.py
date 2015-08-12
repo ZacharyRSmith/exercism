3f196ae75b6c0661b93e33eb9f55d2d0d68d160a
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-12 06:04:38 +0000

    python/meetup: 6th iteration

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,62 +1,47 @@
 from datetime import date
 import calendar
+import string
 
 
 class MeetupDayException(Exception):
     pass
 
 
-def get_day_of_week_int(string):
-    string = string.lower()
-    conversion = {
-        'monday': 0,
-        'tuesday': 1,
-        'wednesday': 2,
-        'thursday': 3,
-        'friday': 4,
-        'saturday': 5,
-        'sunday': 6
-    }
-
-    try:
-        return conversion[string]
-    except:
-        raise MeetupDayException('I did not understand your day! :_(')
-
-
 def match_day(year, month, day_of_week, start):
-    for day_of_month in range(start, start+7):
+    for day_of_month in xrange(start, 32):
         try:
-            d = date(year, month, day_of_month)
+            candidate = date(year, month, day_of_month)
         # ValueError expected when day is out of range for month
         except ValueError:
-            raise MeetupDayException('No day matches your criteria!')
+            raise MeetupDayException('No day matches your criteria! D:')
 
-        if d.weekday() == day_of_week:
-            return d
+        if candidate.weekday() == day_of_week:
+            return candidate
 
 
-def meetup_day(year, month, day_of_week_str, qualifier):
-    day_of_week = get_day_of_week_int(day_of_week_str)
-    qualifier = qualifier.lower()
+def meetup_day(year, month, day_of_week_str, pos):
+    day_of_week = __get_day_of_week_int(day_of_week_str)
+    pos = pos.lower()
+    first_possible_dates = {'1st': 1,
+                            '2nd': 8,
+                            '3rd': 15,
+                            '4th': 22,
+                            '5th': 29,
+                            'teenth': 13,
+                            'last': calendar.monthrange(year, month)[1] - 6}
 
-    if qualifier == '1st':
-        return match_day(year, month, day_of_week, 1)
-    elif qualifier == '2nd':
-        return match_day(year, month, day_of_week, 8)
-    elif qualifier == '3rd':
-        return match_day(year, month, day_of_week, 15)
-    elif qualifier == '4th':
-        return match_day(year, month, day_of_week, 22)
-    elif qualifier == '5th':
-        return match_day(year, month, day_of_week, 29)
-    elif qualifier == 'teenth':
-        return match_day(year, month, day_of_week, 13)
-    elif qualifier == 'last':
-        days_in_month = calendar.monthrange(year, month)[1]
-
-        return match_day(year, month, day_of_week, days_in_month - 6)
-    else:
-        raise MeetupDayException('I did not understand your qualifier! ;_; ' +
-                                 'I need: "teenth", "1st", "2nd", "3rd", ' +
+    if pos not in first_possible_dates:
+        raise MeetupDayException('I did not understand the pos arg, ' + pos +
+                                 '! ;_; '
+                                 'I need: "teenth", "1st", "2nd", "3rd", '
                                  '"4th", "5th", or "last".')
+
+    return match_day(year, month, day_of_week, first_possible_dates[pos])
+
+
+def __get_day_of_week_int(text):
+    try:
+        return list(calendar.day_name).index(string.capitalize(text))
+    except ValueError:
+        raise MeetupDayException('I did not understand your day,'
+                                 ' ' + text + '! :_(')
