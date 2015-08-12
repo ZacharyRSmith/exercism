940e062ebfc795bf058a15d9e2c83e239be90361
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-12 18:46:43 +0000

    javascript/gigasecond: 3rd iteration

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,13 +1,13 @@
-'use strict';
-
 module.exports = function (expulsionDay) {
-    var GIGASECOND = Math.pow(10, 9);
+  'use strict';
 
-    this.date = function () {
-        var happyDay = expulsionDay;
-        happyDay.setSeconds(happyDay.getSeconds() + GIGASECOND);
-        happyDay.setHours(0, 0, 0, 0);
+  var GIGASECOND = Math.pow(10, 9);
 
-        return happyDay;
-    };
+  this.date = function () {
+    var happyDay = expulsionDay;
+    happyDay.setSeconds(happyDay.getSeconds() + GIGASECOND);
+    happyDay.setHours(0, 0, 0, 0);
+
+    return happyDay;
+  };
 };
