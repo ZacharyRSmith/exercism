e5479016f13a6a411a5360098a23e159cb52d0a1
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-12 20:36:09 +0000

    javascript/space-age 3rd: REFACTOR

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,49 +1,28 @@
 var SECONDS_PER_EARTH_YR = 31557600;
 
 function SpaceAge (seconds) {
-  this._seconds = seconds;
+  return {
+    seconds: seconds,
+    onEarth: this.convertYears(1),
+    onMercury: this.convertYears(0.2408467),
+    onVenus: this.convertYears(0.61519726),
+    onMars: this.convertYears(1.8808158),
+    onJupiter: this.convertYears(11.862615),
+    onSaturn: this.convertYears(29.447498),
+    onUranus: this.convertYears(84.016846),
+    onNeptune: this.convertYears(164.79132)
+  };
 }
 
 SpaceAge.prototype = {
   constructor: SpaceAge,
 
   convertYears: function (inEarthYrs) {
-    return +((this.seconds / SECONDS_PER_EARTH_YR) / inEarthYrs).toFixed(2);
+    return function () {
+      return +((this.seconds / SECONDS_PER_EARTH_YR) /
+               inEarthYrs).toFixed(2);
+    };
   },
-
-  get seconds() { return this._seconds; },
-
-  onEarth: function () {
-    return this.convertYears(1);
-  },
-
-  onMercury: function () {
-    return this.convertYears(0.2408467);
-  },
-
-  onVenus: function () {
-    return this.convertYears(0.61519726);
-  },
-
-  onMars: function () {
-    return this.convertYears(1.8808158);
-  },
-
-  onJupiter: function () {
-    return this.convertYears(11.862615);
-  },
-
-  onSaturn: function () {
-    return this.convertYears(29.447498);
-  },
-
-  onUranus: function () {
-    return this.convertYears(84.016846);
-  },
-
-  onNeptune: function () {
-    return this.convertYears(164.79132);
-  }
 };
 
 module.exports = SpaceAge;
