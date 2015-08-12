18117ae2e910c2703c8e58324171962c36c4d4c7
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-12 19:03:35 +0000

    javascript/triangle: 3rd iteration

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,25 +1,19 @@
 function Triangle (a, b, c) {
   this.kind = function () {
-    if (!a > 0 || !b > 0 || !c > 0) {
-      throw new UserException("All sides must have positive length");
-    } else if (a === b && b === c) {
-      return 'equilateral';
-    } else if (!_passesTriangleInequality()) {
-      throw new UserException("Violates the triangle inequality prinzippie");
-    } else if ((a === b || a === c) || (b === c)) {
-      return 'isosceles';
-    } else {
-      return 'scalene';
+    if (a <= 0 || a > (b+c) ||
+        b <= 0 || b > (a+c) ||
+        c <= 0 || c > (a+b)) {
+      throw new UserException('Illegal triangle');
     }
-  };
 
-  var _passesTriangleInequality = function () {
-    if (a > b && a > c) {
-      return (b + c) >= a;
-    } else if (b > a && b > c) {
-      return (a + c) >= b;
-    } else {
-      return (a + b) >= c;
+    if (a === b && b === c) {
+      return 'equilateral';
+    }
+    else if (a === b || a === c || b === c) {
+      return 'isosceles';
+    }
+    else {
+      return 'scalene';
     }
   };
 }
