95f03bd083c578e7d3181deffdbadcc283bc73f0
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-12 04:44:42 +0000

    python/hamming: 3rd iteration

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -3,10 +3,5 @@ def distance(a, b):
         raise ValueError("Hamming Distance is only defined for strands of " +
                          "equal length")
 
-    distance = 0
-
-    for pair in zip(list(a), list(b)):
-        if pair[0] != pair[1]:
-            distance += 1
-
-    return distance
+    return sum(1 for pair in zip(list(a), list(b))
+               if pair[0] != pair[1])
