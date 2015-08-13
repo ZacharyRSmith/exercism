d7005d1775425febfca335275e6a2e34acef086f
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-08-13 19:47:18 +0000

    javascript/nucleotide-count 4th and 5th itr: Change constructor pattern

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,45 +1,22 @@
 function dna (sequence) {
-  return new Acid(sequence);
-}
-
-
-function Acid (sequence) {
-  this.sequence = this.validateSequence(sequence);
-}
-
-Acid.prototype = {
-  constructor: Acid,
-
-  count: function (nucToCount) {
-    var regex = new RegExp(nucToCount, 'g');
-    var matches = this.sequence.match(regex);
-
-    if (!matches) { return 0; }
-
-    return matches.length;
-  },
-
-  histogram: function () {
-    return {
-      A: this.count('A'),
-      T: this.count('T'),
-      C: this.count('C'),
-      G: this.count('G'),
-    };
-  },
-
-  validateSequence: function (sequence) {
-    if (!sequence) { return ''; }
-
-    var match = sequence.match(/[^AGCT]/);
-
-    if (match) {
-        throw new Error("Only 'A', 'G', 'C', and 'T' nucleotides allowed, " +
-                        "but " + match[0] + "was given");
-    }
-
-    return sequence;
+  if (!sequence) {
+    sequence = '';
   }
-};
+  if (sequence.match(/[^AGCT]/)) {
+    throw new Error("Only 'A', 'G', 'C', and 'T' nucleotides allowed, " +
+                    "but " + match[0] + " was given");
+  }
+
+  var histogram_obj = sequence.split('').reduce(function (hist, nucleo) {
+    hist[nucleo] += 1;
+    return hist;
+  }, { 'A': 0, 'G': 0, 'C': 0, 'T': 0 });
+
+  function count (nucleoToCount) { return histogram_obj[nucleoToCount]; }
+
+  function histogram () { return histogram_obj; }
+
+  return { count: count, histogram: histogram };
+}
 
 module.exports = dna;
