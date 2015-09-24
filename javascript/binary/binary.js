ac2f7bf3b7cff015d8eea1edd43541241ee26993
Zachary Ryan Smith <zacharyryansmith@Zacharys-MacBook-Pro.local>
2015-09-24 10:34:18 -0400

    JS binary: Refactor.

--- javascript/binary/binary.js
+++ javascript/binary/binary.js
@@ -1,17 +1,13 @@
 var Binary = function (str) {
   this.toDecimal = function () {
-    var decimal = 0;
+    if (/[^0-1]+/.test(str)) {
+      return 0;
+    }
     var byPowersOfTwo = str.split('').reverse();
 
-    for (var i = 0; i < byPowersOfTwo.length; i++) {
-      if (byPowersOfTwo[i] !== '0' && byPowersOfTwo[i] !== '1') {
-        decimal = 0;
-        return decimal;
-      }
-      decimal += byPowersOfTwo[i] * Math.pow(2, i);
-    }
-
-    return decimal;
+    return byPowersOfTwo.reduce(function (decimal, crnt, i) {
+      return decimal + parseInt(crnt) * Math.pow(2, i);
+    }, 0);
   };
 };
 
