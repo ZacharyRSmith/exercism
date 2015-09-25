a7ff3bd52bd02630275ce36cab0da15a5c14348f
Zachary Ryan Smith <zacharyryansmith@Zacharys-MacBook-Pro.local>
2015-09-25 00:55:29 -0400

    Add JS raindrops.

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,21 @@
+module.exports = function() {
+  this.convert = function(int) {
+    var result = '';
+
+    if (int % 3 === 0) {
+      result += 'Pling';
+    }
+    if (int % 5 === 0) {
+      result += 'Plang';
+    }
+    if (int % 7 === 0) {
+      result += 'Plong';
+    }
+
+    if (result === '') {
+      return '' + int;
+    } else {
+      return result;
+    }
+  };
+};
