5c8c7ea62f02c4527e7cc8da2e35e32ee5537b5b
Zachary Ryan Smith <zacharyryansmith@Zacharys-MacBook-Pro.local>
2015-09-28 22:30:05 -0400

    JS allergies: 2nd iter.

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,29 +1,59 @@
-function Allergies(score) {
-  var allergens = {
-    eggs: 1,
-    peanuts: 2,
-    shellfish: 4,
-    strawberries: 8,
-    tomatoes: 16,
-    chocolate: 32,
-    pollen: 64,
-    cats: 128
-  };
+var ALLERGENS = {
+  1: "eggs",
+  2: "peanuts",
+  4: "shellfish",
+  8: "strawberries",
+  16: "tomatoes",
+  32: "chocolate",
+  64: "pollen",
+  128: "cats"
+};
 
-  var allergies = Object.keys(allergens).reduce(function(result, allergen) {
-    if (allergens[allergen] & score) {
-      result.push(allergen);
+var Allergies = function(score) {
+  return {
+    list: function() {
+      return Object.keys(ALLERGENS).reduce(function(prev, curr) {
+        return (curr & score ? prev.concat(ALLERGENS[curr]) : prev);
+      }, []);
+    },
+    allergicTo: function(allergen) {
+      return (this.list().indexOf(allergen) !== -1);
     }
-    return result;
-  }, []);
-
-  this.allergicTo = function(allergen) {
-    return allergies.indexOf(allergen) !== -1;
   };
-
-  this.list = function() {
-    return allergies;
-  };
-}
+};
 
 module.exports = Allergies;
+
+// 'use strict';
+
+// var allergenScores = {
+//   eggs: 1,
+//   peanuts: 2,
+//   shellfish: 4,
+//   strawberries: 8,
+//   tomatoes: 16,
+//   chocolate: 32,
+//   pollen: 64,
+//   cats: 128
+// };
+
+// function Allergies(score) {
+//   var allergens = Object.keys(allergenScores).reduce(function(res, allergen) {
+//     if (allergenScores[allergen] & score) {
+//       res.push(allergen);
+//     }
+//     return res;
+//   }, []);
+
+//   return {
+//     allergicTo: function(allergen) {
+//       return allergens.indexOf(allergen) !== -1;
+//     },
+
+//     list: function() {
+//       return allergens;
+//     }
+//   };
+// }
+
+// module.exports = Allergies;
