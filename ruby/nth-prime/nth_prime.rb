fbb36e91d25728345930639c334dfe282520b033
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-01-27 12:57:04 -0800

    Improve Ruby nth_prime solution.

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,24 +1,28 @@
 class Prime
+  @@primes = [2]
+
   def self.nth(n)
     if n < 1 || !n.is_a?(Integer)
       raise ArgumentError.new('Argument must be a positive integer.')
     end
+
+    return @@primes[n-1] if @@primes[n-1]
+
     # alt solution:
     # could create a set of numbers, from which we'd remove multiples of primes
 
     # naive solution:
-    i = 2
-    nth = 0
-    primes = []
-    while nth < n
-      if primes.none? { |prime| i % prime == 0 }
-        primes << i
-        nth += 1
-      end
+    num = @@primes[-1]
+    while !@@primes[n-1]
+      @@primes << num if Prime.isnt_multiple_of_cached_primes?(num)
 
-      i += 1
+      num += 1
     end
 
-    primes.pop
+    @@primes[-1]
+  end
+
+  def self.isnt_multiple_of_cached_primes?(num)
+    @@primes.none? { |prime| num % prime == 0 }
   end
 end
