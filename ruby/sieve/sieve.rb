587f3721d895eb806b2e70da2e75c9082e18affb
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-03 07:13:44 -0800

    Improve Ruby sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -14,18 +14,17 @@ class Sieve
   private
 
   def _gen_primes_up_to_limit_inclusive(limit)
-    candidates = Set.new [2]
-    # Add all odd numbers from 3 to limit inclusive
-    candidates.merge((3..limit).step(2).to_a)
+    # 2 and all odd numbers from 3 to limit inclusive
+    candidates = Set.new [2].concat( (3..limit).step(2).to_a )
 
     candidates.each do |candidate|
-      _remove_multiples(candidates, candidate)
+      _remove_multiples(candidates, candidate, limit)
     end
 
     candidates.to_a
   end
 
-  def _remove_multiples(candiates, n)
+  def _remove_multiples(candidates, n, limit)
     ((2*n)..limit).step(n) { |multiple| candidates.delete(multiple) }
   end
 end
