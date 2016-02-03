8c2bfdecc5795e9ebd8b1b4e3686005a01ae0fb0
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-03 06:27:02 -0800

    Clean up Ruby sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -8,19 +8,19 @@ class Sieve
     if !limit.is_a?(Integer) || limit < 2
       raise ArgumentError.new("Sieve's limit must be a positive integer above 1")
     end
-    @primes = _get_primes_up_to_limit_inclusive(limit)
+    @primes = _gen_primes_up_to_limit_inclusive(limit)
   end
 
   private
 
-  def _get_primes_up_to_limit_inclusive(limit)
+  def _gen_primes_up_to_limit_inclusive(limit)
     primes = [2]
     multiples = Set.new [2]
 
     (3..limit).step(2) do |n|
       if !multiples.include?(n)
         primes << n
-        multiples = _mark_multiples(multiples, n, limit)
+        _mark_multiples(multiples, n, limit)
       end
     end
 
@@ -31,7 +31,5 @@ class Sieve
     (start..stop).step(start) do |multiple|
       multiples.add(multiple)
     end
-
-    multiples
   end
 end
