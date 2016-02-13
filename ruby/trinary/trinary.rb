6630e8af87a4070f1b22d8f19d6a042f64bcf7a2
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-13 10:18:42 -0800

    (Ruby) Solve trinary

---
+++ ruby/trinary/trinary.rb
@@ -0,0 +1,22 @@
+class Trinary
+  INVALID_INPUT_RESULT = 0
+
+  def initialize(trinary)
+    @trinary = trinary
+  end
+
+  def to_decimal
+    return INVALID_INPUT_RESULT if /[^0-2]/.match(trinary)
+
+    i = -1
+    trinary.split('').reverse_each.inject(0) do |mem, ltr|
+      i += 1
+      n = ltr.to_i
+      mem += (n * 3**i)
+    end
+  end
+
+  private
+
+    attr_reader :trinary
+end
