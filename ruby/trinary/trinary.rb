4ff13f6312ffc8c5432095a1574289edad7b8b21
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-15 10:22:05 -0800

    (Ruby) Improve solution to trinary

--- ruby/trinary/trinary.rb
+++ ruby/trinary/trinary.rb
@@ -1,20 +1,25 @@
 class Trinary
-  INVALID_INPUT_RESULT = 0
+  INVALID_INPUT_REPLACEMENT = 0.freeze
 
   def initialize(trinary)
-    @trinary = trinary
+    @decimal = _process_input(trinary)
   end
 
+  @property
   def to_decimal
-    return INVALID_INPUT_RESULT if /[^0-2]/.match(trinary)
-
-    trinary.reverse.chars.each_with_index.inject(0) do |mem, (ltr, i)|
-      n = ltr.to_i
-      val = n * 3**i
-      mem += val
-    end
+    decimal
   end
 
   private
-    attr_reader :trinary
+    attr_reader :decimal
+
+    def _process_input(trinary)
+      return INVALID_INPUT_REPLACEMENT if /[^0-2]/.match(trinary)
+
+      trinary.reverse.chars.each_with_index.inject(0) do |mem, (ltr, i)|
+        n = ltr.to_i
+        val = n * 3**i
+        mem += val
+      end
+    end
 end
