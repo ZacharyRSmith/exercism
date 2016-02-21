afd18fafdfef874f339f46fa469831f94ff67230
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-21 10:59:16 -0800

    (Ruby) improve solution to anagram

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -4,28 +4,19 @@ class Anagram
     @original_word_sorted = _sort(original_word_normalized)
   end
 
-  def match(candidates)
-    return candidates.inject([]) do |anagrams, candidate|
-      anagrams << candidate if _is_anagram(candidate)
-      anagrams
-    end
-  end
+  def match(candidates) candidates.select { |c| _is_anagram?(c) } end
 
   private
     attr_reader :original_word_normalized
     attr_reader :original_word_sorted
 
-    def _is_anagram(candidate)
+    def _is_anagram?(candidate)
       candidate_normalized = _normalize(candidate)
       return false if candidate_normalized == original_word_normalized
       _sort(candidate_normalized) == original_word_sorted
     end
 
-    def _normalize(arg)
-      arg.downcase
-    end
+    def _normalize(arg) arg.downcase end
 
-    def _sort(word)
-      word.chars.sort
-    end
+    def _sort(word) word.chars.sort end
 end
