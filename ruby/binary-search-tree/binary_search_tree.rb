1ce9aa2f01af000b83a7c105b2f936ebeaffaaae
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-22 08:50:05 -0800

    (Ruby) Improve solution to binary_search_tree

--- ruby/binary-search-tree/binary_search_tree.rb
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -7,26 +7,29 @@ class Bst
   attr_reader :data
 
   def each(&block)
-    @children['left'].each(&block) if @children['left']
+    children['left'].each(&block) if children['left']
     yield @data
-    @children['right'].each(&block) if @children['right']
+    children['right'].each(&block) if children['right']
   end
 
   def left
-    @children['left']
+    children['left']
   end
   
   def insert(newData)
     side = (newData > data ? 'right' : 'left')
 
-    if @children[side]
-      @children[side].insert(newData)
+    if children[side]
+      children[side].insert(newData)
     else
-      @children[side] = self.class.new(newData)
+      children[side] = self.class.new(newData)
     end
   end
 
   def right
-    @children['right']
+    children['right']
   end
+
+  private
+    attr_reader :children
 end
