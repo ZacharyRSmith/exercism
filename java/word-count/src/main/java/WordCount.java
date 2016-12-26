6a5768c30ce821a8c72bcbe853fee2bc0b755791
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-12-25 21:20:57 -0500

    Pass first test in Java WordCount

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,2 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount {
+  public Map<String, Integer> Phrase(String input) {
+    Map<String, Integer> res = new HashMap<String, Integer>();
+
+    res.put(input, 1);
+
+    return res;
+  }
 }
