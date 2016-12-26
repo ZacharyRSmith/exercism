67812091b6786e3357aac06ba811e7cb4dae44ee
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-12-25 21:33:41 -0500

    complete Java WordCount

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -5,7 +5,14 @@ public class WordCount {
   public Map<String, Integer> Phrase(String input) {
     Map<String, Integer> res = new HashMap<String, Integer>();
 
-    res.put(input, 1);
+    String[] ary = input.toLowerCase().split("\\W+");
+
+    for (String word : ary) {
+      if (!res.containsKey(word)) res.put(word, 0);
+      int crnt = res.get(word);
+
+      res.put(word, crnt + 1);
+    }
 
     return res;
   }
