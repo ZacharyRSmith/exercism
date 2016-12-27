23a44eaf76f052a78abef267394d804385b25a25
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-12-27 18:10:36 -0500

    Iterate on Java Triangle

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -2,10 +2,14 @@
  * Tells type of triangle.
  */
 public class Triangle {
+  private double x, y, z;
   private TriangleKind kind;
 
   Triangle(double x, double y, double z) throws TriangleException {
-    if (!isValidInput(x, y, z)) throw new TriangleException();
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    if (isInvalidTriangle()) throw new TriangleException();
     setKind(x, y, z);
   }
 
@@ -13,14 +17,14 @@ public class Triangle {
     return this.kind;
   }
 
-  private boolean isValidInput(double x, double y, double z) {
-    if (x <= 0 || y <= 0 || z <= 0) return false;
+  private boolean isInvalidTriangle() {
+    boolean sideMissing = x <= 0 || y <= 0 || z <= 0;
     // The following line validates the triangle inequality theorem: z ≤ x + y
     // where x,y, and z are the lengths of the sides of a triangle. In other words, the
     // sum of the lengths of any two sides of a triangle always exceeds or is equal to
     // the length of the third side.
-    if (x > y + z || y > x + z || z > x + y) return false;
-    return true;
+    boolean violatesTriangleTheorem = x > y + z || y > x + z || z > x + y;
+    return sideMissing || violatesTriangleTheorem;
   }
 
   private void setKind(double x, double y, double z) {
