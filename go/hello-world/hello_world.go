06af070a8ef66c846c7c8dcd21363afd2ce4f157
Zachary R. Smith <ZacharySmith4989@gmail.com>
2017-07-04 11:44:09 -0400

    change: Update with fetched changes

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -32,11 +32,9 @@ const testVersion = 4
 // the function. It is recommended good practice, but, as we mention above, it's
 // not necessary for making your solutions pass.
 // https://github.com/golang/go/wiki/CodeReviewComments#comment-sentences
-func HelloWorld() string {
-	// Write some code here to pass the test suite.
-
-	// When you have a working solution, REMOVE ALL THE STOCK COMMENTS.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers will protest!
-	return ""
+func HelloWorld(name string) string {
+  if name == "" {
+    name = "World"
+  }
+  return "Hello, " + name + "!"
 }
