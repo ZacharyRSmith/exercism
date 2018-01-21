9720716f148341a50c65a64362854597d02c0eb2
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-01-21 11:05:49 -0500

    solution 2: python - isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,11 +1,9 @@
-import re
+def is_isogram(string = ''):
+    if (not string): return True
+    letters = [c for c in string.lower() if c.isalpha()]
 
-def is_isogram(input = ''):
-    if (not input): return True
-    input = re.sub(r'\_*\W*\d*', '', input).lower()
-
-    s = set(input[0])
-    for ch in input[1:]:
+    s = set(letters[0])
+    for ch in letters[1:]:
         if (ch in s): return False
         s.add(ch)
 
