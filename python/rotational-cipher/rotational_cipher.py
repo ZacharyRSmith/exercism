9422b2013874f1b6ad24a8a4147e9183fbe134c2
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-01-21 16:54:12 -0500

    solution 1: python - rotational_cipher

---
+++ python/rotational-cipher/rotational_cipher.py
@@ -0,0 +1,21 @@
+def rotate(text: str, key: int) -> str:
+    # print(ord('a')) # 97
+    # print(ord('z')) # 122
+    # print(ord('A')) # 65
+    # print(ord('Z')) # 90
+    res = ''
+    def shouldWrap(o):
+        isLowerCase = o <= 90
+        return (isLowerCase and o + key > 90) or (not isLowerCase and o + key > 122)
+
+    for ch in text:
+        if not ch.isalpha():
+            res += ch
+            continue
+        o = ord(ch)
+        if (shouldWrap(o)):
+            res += chr(o + key - 26)
+        else:
+            res += chr(o + key)
+
+    return res
