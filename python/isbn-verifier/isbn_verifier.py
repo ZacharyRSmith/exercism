cbf3d8a42bc0473a378546aa6e8ceacb06fa39e9
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-01-21 16:21:41 -0500

    solution 2: python - isbn_verifier

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -5,14 +5,16 @@ import re
 # @param {string} isbn: ISBN to be verified, with or without separating dashes.
 def verify(isbn):
     if not isbn: return False
-    numbers = [n for n in isbn if n.isdigit()]
-    hasX = re.search(r'[\d|-]X{1}$', isbn)
-    if len(numbers) != 10:
-        if len(numbers) != 9 or not hasX: return False
+    clean_isbn = re.sub('[^0-9X]', '', isbn)
+    if len(clean_isbn) != 10: return False
+    if re.search(r'X.+', clean_isbn): return False
     res = 0
 
-    for i in range(len(numbers)):
-        res = res + int(numbers[i]) * (10 - i)
-    if hasX: res += 10
+    for i in range(9):
+        res += int(clean_isbn[i]) * (10 - i)
+    if clean_isbn[9] == 'X':
+        res += 10
+    else:
+        res += int(clean_isbn[9])
 
     return res % 11 == 0
