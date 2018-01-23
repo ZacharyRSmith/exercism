101bc947d505c87b5a011c2306c461e90aa76f22
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-01-22 23:21:02 -0500

    solution 2: python - etl

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,8 +1,6 @@
 def transform(legacy_data):
-    res = {}
-
-    for score in legacy_data.keys():
-        for word in legacy_data[score]:
-            res[word.lower()] = score
-
-    return res
+    return {
+        letter.lower(): point
+        for point, letters in legacy_data.items()
+        for letter in letters
+    }
