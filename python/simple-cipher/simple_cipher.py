054d124989c4fb3197463c889e32f397092f775a
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-01-24 23:02:19 -0500

    solution 2: python - simple_cipher

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,9 +1,16 @@
 import re
+import secrets
+import string
 
 CEASAR_SHIFT_BY = 3
 ORD_FOR_a = 97
 ORD_FOR_z = 122
 NUM_LETTERS = 26
+
+
+def gen_key():
+    return ''.join([secrets.choice(string.ascii_lowercase)
+                    for i in range(100 + secrets.randbelow(50))])
 
 
 def shift(ch, shift_by):
@@ -15,37 +22,40 @@ def shift(ch, shift_by):
     return chr(o)
 
 
+def clean(text):
+    return re.sub(r'[^a-zA-Z]', '', text).lower()
+
+
 class Cipher(object):
-
-    def __init__(self, key='d'*100):
-        if re.search(r'[^a-z]', key):
+    def __init__(self, key=gen_key()):
+        if not all(ch.isalpha() and ch.islower() for ch in key):
             raise ValueError(f'key must be all lowercase letters, but received "{key}".')
-        self.key = key
+        self.secret = key
 
-    def encode(self, text):
-        return ''.join(shift(text[i], self._get_shift(i))
-                       for i in range(len(text)))
+    @property
+    def key(self):
+        return self.secret
+
+    def encode(self, input):
+        cleaned = clean(input)
+        return ''.join(shift(cleaned[i], self._get_shift(i))
+                       for i in range(len(cleaned)))
 
     def decode(self, text):
         return ''.join(shift(text[i], -self._get_shift(i))
                        for i in range(len(text)))
 
     def _get_shift(self, i):
-        while i >= len(self.key):
-            i -= len(self.key)
+        i = i % len(self.key) if i >= 0 else i % -len(self.key)
         return ord(self.key[i]) - ORD_FOR_a
 
 
 class Caesar(object):
-
     def __init__(self):
-        pass
+        self.cipher = Cipher('d')
 
     def decode(self, input):
-        return ''.join(shift(ch, -CEASAR_SHIFT_BY) for ch in self._clean(input))
+        return self.cipher.decode(input)
 
     def encode(self, input):
-        return ''.join(shift(ch, CEASAR_SHIFT_BY) for ch in self._clean(input))
-
-    def _clean(self, text):
-        return re.sub(r'[^a-zA-Z]', '', text).lower()
+        return self.cipher.encode(input)
