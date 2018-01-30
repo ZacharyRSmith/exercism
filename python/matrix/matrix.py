9bb9c5466095f98462c4c5423857977e6929b13e
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-01-30 17:58:39 -0500

    solution 2: python - matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,11 +1,12 @@
 import re
+import numpy as np
 
 
 class Matrix(object):
 
     def __init__(self, matrix_string):
-        self.matrix_string = matrix_string
-        self.rows = [re.split(r'\D+', r) for r in matrix_string.split('\n')]
-        for i in range(len(self.rows)):
-            self.rows[i] = [int(num) for num in self.rows[i]]
-        self.columns = [list(column) for column in zip(*self.rows)]
+        self.rows = [[int(num) for num in re.split(r'\D+', r)]
+                     for r in matrix_string.splitlines()]
+        self.columns = np.array(self.rows).T.tolist()
+        # If you don't want to use numpy:
+        # self.columns = [list(column) for column in zip(*self.rows)]
