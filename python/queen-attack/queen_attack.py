8e5ff5a22d17dcad690759f3380efd670050b45e
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-02-01 21:02:59 -0500

    solution 2: python - queen_attack

--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -1,7 +1,3 @@
-def add(coord, change):
-    return tuple(sum(x) for x in zip(coord, change))
-
-
 def is_valid_coord(coord):
     return all(0 <= x and x <= 7 for x in coord)
 
@@ -9,7 +5,7 @@ def is_valid_coord(coord):
 def _validate(white_position, black_position):
     if white_position == black_position:
         raise ValueError(f'Queens cannot be on the same position')
-    if not all(is_valid_coord(pos) for pos in [white_position, black_position]):
+    if not all(is_valid_coord(coord) for coord in [white_position, black_position]):
         raise ValueError(f'Invalid position')
 
 
@@ -23,22 +19,12 @@ def board(white_position, black_position):
     return bord
 
 
-def diagonal_squares(input_coord):
-    for direction in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
-        coord = input_coord
-        while is_valid_coord(coord):
-            coord = add(coord, direction)
-            if is_valid_coord(coord):
-                yield coord
-
-
-def is_same_diagonal(white_position, black_position):
-    return any(diagonal_square == black_position
-               for diagonal_square in diagonal_squares(white_position))
+def is_same_diagonal(wi, wj, bi, bj):
+    return abs(wi - bi) == abs(wj - bj)
 
 
 def can_attack(white_position, black_position):
     _validate(white_position, black_position)
     wi, wj = white_position
     bi, bj = black_position
-    return wi == bi or wj == bj or is_same_diagonal(white_position, black_position)  # noqa
+    return wi == bi or wj == bj or is_same_diagonal(*white_position, *black_position)  # noqa
