2f68ab8f2b8f8605bbb91971ff2f3ea9c9c9ee45
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-02-01 21:11:59 -0500

    solution 3: python - queen_attack

--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -2,29 +2,27 @@ def is_valid_coord(coord):
     return all(0 <= x and x <= 7 for x in coord)
 
 
-def _validate(white_position, black_position):
-    if white_position == black_position:
+def _validate_positions(w_pos, b_pos):
+    if w_pos == b_pos:
         raise ValueError(f'Queens cannot be on the same position')
-    if not all(is_valid_coord(coord) for coord in [white_position, black_position]):
+    if not all(is_valid_coord(coord) for coord in [w_pos, b_pos]):
         raise ValueError(f'Invalid position')
 
 
-def board(white_position, black_position):
-    _validate(white_position, black_position)
-    bord = ['_'*8 for i in range(8)]
-    i, j = white_position
-    bord[i] = bord[i][:j] + 'W' + bord[i][j+1:]
-    i, j = black_position
-    bord[i] = bord[i][:j] + 'B' + bord[i][j+1:]
-    return bord
+def board(w_pos, b_pos):
+    _validate_positions(w_pos, b_pos)
+    _board = [['_']*8 for _ in range(8)]
+    _board[w_pos[0]][w_pos[1]] = 'W'
+    _board[b_pos[0]][b_pos[1]] = 'B'
+    return [''.join(row) for row in _board]
 
 
 def is_same_diagonal(wi, wj, bi, bj):
     return abs(wi - bi) == abs(wj - bj)
 
 
-def can_attack(white_position, black_position):
-    _validate(white_position, black_position)
-    wi, wj = white_position
-    bi, bj = black_position
-    return wi == bi or wj == bj or is_same_diagonal(*white_position, *black_position)  # noqa
+def can_attack(w_pos, b_pos):
+    _validate_positions(w_pos, b_pos)
+    wi, wj = w_pos
+    bi, bj = b_pos
+    return wi == bi or wj == bj or is_same_diagonal(*w_pos, *b_pos)
