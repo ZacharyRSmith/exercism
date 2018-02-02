923ae1114ebb75510b00c98d8ceb2bc79bb11fef
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-02-01 19:56:09 -0500

    solution 2?: python - saddle_points

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,31 +1,24 @@
-def saddle_points(matrix):
-    def _validate_matrix():
-        for i in range(len(matrix)):
-            if len(matrix[i]) != len(matrix):
-                raise ValueError(f'rows and cols must all be the same length.')
+def cells(matrix):
+    if not len(matrix) or not len(matrix[0]):
+        return
+    height, width = len(matrix), len(matrix[0])
+    for i in range(height):
+        for j in range(width):
+            yield matrix[i][j], i, j
 
-    def _gteAllOnRow(i, j, val):
-        for otherJ in range(len(matrix[0])):
-            if otherJ == j:
-                continue
-            if val < matrix[i][otherJ]:
-                return False
-        return True
 
-    def _lteAllOnCol(i, j, val):
-        for otherI in range(len(matrix)):
-            if otherI == i:
-                continue
-            if val > matrix[otherI][j]:
-                return False
-        return True
-
-    _validate_matrix()
-    results = set()
+def col(matrix, j):
     for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            val = matrix[i][j]
-            if _gteAllOnRow(i, j, val) and _lteAllOnCol(i, j, val):
-                results.add((i, j))
+        yield matrix[i][j]
 
-    return results
+
+def _validate_matrix(matrix):
+    for i in range(len(matrix)):
+        if len(matrix[i]) != len(matrix):
+            raise ValueError(f'rows and cols must all be the same length.')
+
+
+def saddle_points(matrix):
+    _validate_matrix(matrix)
+    return set((i, j) for cell, i, j in cells(matrix)
+               if max(matrix[i]) == cell and min(col(matrix, j)) == cell)
