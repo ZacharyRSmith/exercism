e3727f37f02193f7dc1a34bc1dbcb1c8cb731e1e
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-02-26 18:15:31 -0600

    iter: python - anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,3 +1,14 @@
-def detect_anagrams(word, candidate_list):
-    return filter(lambda c: (word.lower() != c.lower()) and
-                  (sorted(word.lower()) == sorted(c.lower())), candidate_list)
+def histogram(word):
+    res = {}
+    for ch in word:
+        if ch not in res:
+            res[ch] = 0
+        res[ch] += 1
+    return res
+
+
+def detect_anagrams(word, words):
+    word_histogram = histogram(word.lower())
+    word_lowered = word.lower()
+    return [w for w in words
+            if w.lower() != word_lowered and histogram(w.lower()) == word_histogram]
