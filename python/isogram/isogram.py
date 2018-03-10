3f5ad567356baa19f66f0f346bd55c3b17efe91b
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-03-10 14:40:59 -0500

    iter: python - isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,4 +1,17 @@
-def is_isogram(string = ''):
+def is_isogram(string=''):
+    '''Determine if a word or phrase is an isogram.
+
+    An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter.
+
+    Examples of isograms:
+
+    - lumberjacks
+    - background
+    - downstream
+
+    The word *isograms*, however, is not an isogram, because the s repeats.
+    '''
     for i in range(ord('a'), ord('z')):
-        if string.lower().count(chr(i)) > 1: return False
+        if string.lower().count(chr(i)) > 1:
+            return False
     return True
