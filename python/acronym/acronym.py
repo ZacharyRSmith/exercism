21e6cd32cbb273a6e264bef05de95672c3727f14
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-03-18 09:22:57 -0400

    iter: python - acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+
+"""Convert a phrase to its acronym"""
+
 import re
 
 
 def abbreviate(words: str) -> str:
-    return ''.join(word[0] for word in re.split('\W*', words)).upper()
+    """Convert a phrase to its acronym"""
+    return ''.join(word[0] for word in re.split('\W+', words)).upper()
