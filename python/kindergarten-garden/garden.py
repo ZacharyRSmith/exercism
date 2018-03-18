e23cd6bb09141c42e57789330e7c0f53755561ad
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-03-18 10:02:52 -0400

    iter: python - garden

--- python/kindergarten-garden/garden.py
+++ python/kindergarten-garden/garden.py
@@ -1,5 +1,6 @@
-import operator
+#!/usr/bin/env python3
 
+"""Includes data and a Class for a kindergarten's garden."""
 
 PLANTS = {
     'C': 'Clover',
@@ -13,11 +14,33 @@ STUDENTS = ['Alice', 'Bob', 'Charlie', 'David', 'Eve',
 
 
 class Garden(object):
+    """Class representing a kindergarten's garden.
+
+    Given @rows, can tell you which plants each child in the kindergarten class
+    is responsible for.
+    """
+
     def __init__(self, rows, students=STUDENTS):
-        r0, r1 = rows.split()
-        plants = [[PLANTS[key] for key in r0[i:i + 2] + r1[i:i + 2]]
-                  for i in range(0, len(r0), 2)]
+        """
+        Sets self.garden_map.
+
+        @type rows: string
+        @param rows: plants from left-to-right.
+        Rows are separated by `\n`.
+        Starts with the row nearest the windows.
+        Letters represent
+
+        @type students: string
+        @param students: students whose plants are in this garden.
+        """
+        row_0, row_1 = rows.split()
+
+        def student_plants(i):
+            return [PLANTS[key] for key in row_0[i:i + 2] + row_1[i:i + 2]]
+        plants = [student_plants(student_idx)
+                  for student_idx in range(0, len(row_0), 2)]
         self.garden_map = dict(zip(sorted(students), plants))
 
     def plants(self, student):
+        """Returns the plants planted by @student."""
         return self.garden_map[student]
